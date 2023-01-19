fe08189445de5552383d07f43c9d166247db6d5f
AchillesHam <aleahjham@gmail.com>
2023-01-18 16:54:12 -0800

    Add PID controllers with correct Units to SwerveModule

--- src/main/java/frc/robot/subsystems/SwerveModule.java
+++ src/main/java/frc/robot/subsystems/SwerveModule.java
@@ -5,6 +5,7 @@ import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.kinematics.SwerveModuleState;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 
+import com.ctre.phoenixpro.hardware.CANcoder;
 import com.revrobotics.AbsoluteEncoder;
 import com.revrobotics.CANSparkMax;
 import com.revrobotics.CANSparkMaxLowLevel.MotorType;
@@ -18,7 +19,8 @@ public class SwerveModule extends SubsystemBase {
 	private final CANSparkMax angularMotor;
 	private final CANSparkMax velocityMotor;
 
-	private final AbsoluteEncoder angularMotorEncoder;
+	private final CANcoder angularEncoder;
+	private final CANcoder velocityEncoder;
 
 	private final PIDController angularPIDController;
 	private final PIDController velocityPIDController;
@@ -31,7 +33,9 @@ public class SwerveModule extends SubsystemBase {
 	// Parameters: Device Id for Angular Motor, Device Id for Velocity Motor
 	public SwerveModule(
 			int angularMotorDeviceID,
-			int velocityMotorDeviceID) {
+			int velocityMotorDeviceID,
+			int angularEncoderDeviceID,
+			int velocityEncoderDeviceID) {
 
 		angularPIDController = new PIDController(
 				SwerveDriveConstants.ANGULAR_PID_P,
@@ -46,14 +50,22 @@ public class SwerveModule extends SubsystemBase {
 		angularMotor = new CANSparkMax(angularMotorDeviceID, MotorType.kBrushless);
 		velocityMotor = new CANSparkMax(velocityMotorDeviceID, MotorType.kBrushless);
 
-		angularMotorEncoder = angularMotor.getAbsoluteEncoder(Type.kDutyCycle);
-		angularMotorEncoder.setPositionConversionFactor(SwerveDriveConstants.ENCODER_POSITION_CONVERSION_FACTOR);
+		angularEncoder = new CANcoder(angularEncoderDeviceID);
+		velocityEncoder = new CANcoder(velocityEncoderDeviceID);
+
 	}
 
 	// Returns the Rotation of the Angular Motor
-	// In Radians
+	// Returns the roations in number of rotations (eg. instead of 2pi or 360 it returns 1)
 	public double getRotation() {
-		return angularMotorEncoder.getPosition();
+		return angularEncoder.getPosition().getValue() * SwerveDriveConstants.ANGULAR_MOTOR_GEAR_RATIO;
+	}
+
+	// Returns the velocity of the Velocity motor in meters / second of the wheel
+	public double getVelocity() {
+		return velocityEncoder.getVelocity().getValue() *
+				SwerveDriveConstants.VELOCITY_MOTOR_GEAR_RATIO *
+				SwerveDriveConstants.WHEEL_CIRCUMFERENCE;
 	}
 
 	// Sets the speed of the velocity motor and sets the desired state of the angular motor
@@ -61,11 +73,18 @@ public class SwerveModule extends SubsystemBase {
 	// Parameters: disered swerve module state
 	public void setSwerveModuleState(SwerveModuleState desiredState) {
 		state = SwerveModuleState.optimize(desiredState, new Rotation2d(getRotation()));
+
+		angularPIDController.setSetpoint(state.angle.getRotations());
+		velocityPIDController.setSetpoint(state.speedMetersPerSecond);
 	}
 
 	@Override
 	public void periodic() {
-		SmartDashboard.putNumber("Encoder Value", angularMotorEncoder.getPosition());
+		angularMotor.set(
+				angularPIDController.calculate(getRotation()));
+
+		velocityMotor.set(
+				velocityPIDController.calculate(getVelocity()));
 	}
 
 }
