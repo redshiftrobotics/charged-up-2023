ca464c6569f971560904d696e2e0fc0788335394
JohndisHaver <121990902+JohndisHaver@users.noreply.github.com>
2023-01-25 17:35:45 -0800

    Belt Subsystem .Java
    adds motor subsystem for top of robo jabba the hut
    
    Co-Authored-By: Arnold-Tian <75269639+Arnold-Tian@users.noreply.github.com>

---
+++ src/main/java/frc/robot/subsystems/Belt.java
@@ -0,0 +1,31 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.SparkMaxAbsoluteEncoder;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.CommandBase;
+
+public class Belt extends SubsystemBase {
+	private final CANSparkMax BeltMotor;
+
+	public Belt(int motorId) {
+		BeltMotor = new CANSparkMax(motorId, MotorType.kBrushless);
+	}
+
+	//stops the belt motor
+	public void setBeltStop() {
+		BeltMotor.set(0);
+	}
+
+	//Brings items up the belt using the motor
+	public void setBeltIn() {
+		BeltMotor.set(1);
+	}
+
+	//if an item is stuck, use this to reverce the belt
+	public void setBeltOut() {
+		BeltMotor.set(-1);
+	}
+}
