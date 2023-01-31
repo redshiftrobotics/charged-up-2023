77793d701f9129d3ab6bca9200e46594677c7173
Aleah H <aleahjham@gmail.com>
2023-01-30 17:49:55 -0800

    Rename the imports and package names for the deprecated subsystems and commands

--- src/main/java/frc/robot/commands/depricatedCommands/ToggleIntakeDirectionCommand.java
+++ src/main/java/frc/robot/commands/depricatedCommands/ToggleIntakeDirectionCommand.java
@@ -2,9 +2,9 @@
 // Open Source Software; you can modify and/or share it under the terms of
 // the WPILib BSD license file in the root directory of this project.
 
-package frc.robot.commands;
+package frc.robot.commands.depricatedCommands;
 
-import frc.robot.subsystems.Intake;
+import frc.robot.subsystems.depricatedSubsystem.Intake;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 
 /** An example command that uses an example subsystem. */
