3a718de1e1a0a89c913cdd311c67dd680193b9d4
Josh <58442585+JoshFarkas@users.noreply.github.com>
2023-02-10 12:58:47 -0800

    Add TimeFunction to helpers

--- src/main/java/frc/robot/helpers/Helpers.java
+++ src/main/java/frc/robot/helpers/Helpers.java
@@ -1,6 +1,12 @@
 package frc.robot.helpers;
 
 public class Helpers {
+
+	/**
+	 * A helper function that runs and times another method.
+	 * @param func a Runnable to run and time, pass this via ClassName::FunctionName or a lambda.
+	 * @return the time in milliseconds that the function took to run.
+	 */
 	public static long TimeFunction(Runnable func) {
 		long startTime = System.currentTimeMillis();
 		func.run();
