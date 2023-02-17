6018a113a50388b472709f31c1b1a436fb073a9b
DigitalFork <113265535+DigitalFork@users.noreply.github.com>
2023-02-16 17:01:35 -0800

    Squashed commit of the following:
    commit 45cca68123dfc06c749ff7ea535ff2ac2c3ee1e3
    Author: DigitalFork <113265535+DigitalFork@users.noreply.github.com>
    Date:   Thu Feb 16 16:52:51 2023 -0800
    
        Testing changes - switching devices
    
    commit 7701df25529545757c4fb2e3f1a120f10bf3d4d3
    Author: DigitalFork <113265535+DigitalFork@users.noreply.github.com>
    Date:   Thu Feb 16 16:03:01 2023 -0800
    
        Clean RobotContainer
    
        Uncomment code commented for testing
        Remove intake code
        Remove repeated lines & definitions
    
    commit ac43767c39c6cd50646c70f4b7fcd22f19892e28
    Author: DigitalFork <113265535+DigitalFork@users.noreply.github.com>
    Date:   Wed Feb 15 17:55:52 2023 -0800
    
        Comment and prune SwerveModule
    
        RobotContainer has commented code is not ready to merge.
    
        Co-Authored-By: Aleah H <77556991+MeowAleah@users.noreply.github.com>
    
    commit a0184e78773353907d1b44556208ab7551df96b5
    Merge: 6873a2e eba3537
    Author: Aleah H <aleahjham@gmail.com>
    Date:   Wed Feb 15 16:16:17 2023 -0800
    
        Merge branch 'pid-tuning' of https://github.com/redshiftrobotics/charged-up-2023 into pid-tuning
    
    commit eba35377498d56b8c712f38eda3b3b874691b8f8
    Author: DigitalFork <113265535+DigitalFork@users.noreply.github.com>
    Date:   Wed Feb 15 15:33:16 2023 -0800
    
        Comment out disfunctional arm code
    
    commit 6873a2ef4fba918cf19988a21dc029158edaddec
    Author: Aleah H <aleahjham@gmail.com>
    Date:   Sat Feb 11 15:41:56 2023 -0800
    
        Use Command to set state of swerve module WORKING CODE
    
    commit fb08827d99b8bf6c05ccc78a54d5a2cf7337ac78
    Author: DigitalFork <113265535+DigitalFork@users.noreply.github.com>
    Date:   Sat Feb 11 11:59:54 2023 -0800
    
        Add Shuffleboard folder/layout
    
    commit 67b881ca703c27e9b204974460ddf657bb987baa
    Author: Aleah H <aleahjham@gmail.com>
    Date:   Thu Feb 9 17:21:49 2023 -0800
    
        Add CanSparkMax PID loop for velocity, WORKING CODE
    
    commit c457cee2fb149f1b3f9b7a96608f053e72064790
    Author: Aleah H <aleahjham@gmail.com>
    Date:   Wed Feb 8 17:49:34 2023 -0800
    
        Edit PId values and werve velocity code
    
    commit a2219c8fe28baab6c032b75cd84c1e9b4ced0fb2
    Author: Aleah H <aleahjham@gmail.com>
    Date:   Mon Feb 6 17:48:02 2023 -0800
    
        Edit PID constants
    
    commit 021b951bb757ae3fcb1db3d0d34c879fc600f969
    Author: malochite <121986501+malochite@users.noreply.github.com>
    Date:   Mon Jan 30 17:30:18 2023 -0800
    
        Update SwerveModule.java
    
    commit d9c4a96127b3ef0996d88cdf6a575cf3ab2e8269
    Author: malochite <121986501+malochite@users.noreply.github.com>
    Date:   Mon Jan 30 17:24:14 2023 -0800
    
        Update SwerveModule.java
    
    commit 4612d7b7206a857a5e810d622c38e5137df45d24
    Author: malochite <121986501+malochite@users.noreply.github.com>
    Date:   Mon Jan 30 17:20:04 2023 -0800
    
        Update SwerveModule.java
    
    commit e0a8d425a8ddcec0c762b777597b617bc6addca7
    Author: malochite <121986501+malochite@users.noreply.github.com>
    Date:   Mon Jan 30 17:06:41 2023 -0800
    
        Update SwerveModule.java
    
    commit 08e4e56b9195c35ec3aeef97c18ec99456949461
    Author: malochite <121986501+malochite@users.noreply.github.com>
    Date:   Mon Jan 30 16:54:08 2023 -0800
    
        Update SwerveModule.java
    
    commit 62ae6453d0882578ad625331d8cc6c862f4b5c87
    Author: Aleah H <aleahjham@gmail.com>
    Date:   Sat Jan 28 15:29:17 2023 -0800
    
        Edit PID values
    
    commit 9458d5309c141ba7230f0b3d0c9100af73d1eeb6
    Author: Aleah H <aleahjham@gmail.com>
    Date:   Sat Jan 28 14:36:51 2023 -0800
    
        Added config to encoder, enabled continuous input, and redid the state setting code
    
        Working currently:
        State setting
        Angular encoder pid loop
        default setpoint 0
        singularswervemdulesetstate command

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -4,7 +4,6 @@
 
 package frc.robot;
 
-import frc.robot.Constants.IntakeConstants;
 import frc.robot.Constants.OperatorConstants;
 import frc.robot.Constants.SwerveDriveConstants;
 import frc.robot.commands.Autos;
@@ -32,8 +31,6 @@ import edu.wpi.first.wpilibj2.command.RunCommand;
 import edu.wpi.first.wpilibj2.command.button.CommandJoystick;
 import edu.wpi.first.wpilibj2.command.button.CommandXboxController;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
-// import frc.robot.commands.ToggleIntakeCommand;
-// import frc.robot.subsystems.Intake;
 
 /**
  * This class is where the bulk of the robot should be declared. Since Command-based is a
@@ -50,23 +47,19 @@ public class RobotContainer {
 			SwerveDriveConstants.VELOCITY_MOTOR_ID,
 			SwerveDriveConstants.ANGULAR_MOTOR_ENCODER_ID);
 
-	private final AHRS gyro = new AHRS(I2C.Port.kMXP);
-	// link for gyro https://pdocs.kauailabs.com/navx-mxp/software/roborio-libraries/java/
-	private final SwerveDrivetrain drivetrain = new SwerveDrivetrain(gyro);
+	// private final AHRS gyro = new AHRS(I2C.Port.kMXP);
+	// // link for gyro https://pdocs.kauailabs.com/navx-mxp/software/roborio-libraries/java/
+	// private final SwerveDrivetrain drivetrain = new SwerveDrivetrain(gyro);
 
 	private final Command setModule = new SingularSwerveModuleCommand(module1, Math.PI / 2, 1);
 	private final Command zeroModule = new SingularSwerveModuleCommand(module1, 0, 0);
-	private final Command stopCommand = new StopCommand(drivetrain);
-	private final Command driveDistanceTest = new DriveDistanceCommand(drivetrain, new Translation2d(1, 1), true);
-	private final Command driveDurationTest = new DriveDurationCommand(drivetrain, 3,
-			new ChassisSpeeds(1, 0, Math.PI * 2));
-	private final Command rotateTest = new RotateByCommand(drivetrain, new Rotation2d(Math.PI / 2));
-	// Replace with CommandPS4Controller or CommandJoystick if needed
-	// private final CommandXboxController driverController = new CommandXboxController(
-	// 		OperatorConstants.DRIVER_CONTROLLER_PORT);
+	// private final Command stopCommand = new StopCommand(drivetrain);
+	// private final Command driveDistanceTest = new DriveDistanceCommand(drivetrain, new Translation2d(1, 1), true);
+	// private final Command driveDurationTest = new DriveDurationCommand(drivetrain, 3,
+	// 		new ChassisSpeeds(1, 0, Math.PI * 2));
+	// private final Command rotateTest = new RotateByCommand(drivetrain, new Rotation2d(Math.PI / 2));
 
 	private final CommandJoystick driverJoystick = new CommandJoystick(OperatorConstants.DRIVER_JOYSTICK_PORT);
-	// private final Intake intake = new Intake(IntakeConstants.TOP_MOTOR_ID, IntakeConstants.BOTTOM_MOTOR_ID);
 
 	// private final Command toggleFieldRelative = new RunCommand(drivetrain::toggleFieldRelative, drivetrain);
 
@@ -94,19 +87,20 @@ public class RobotContainer {
 		// Schedule `ExampleCommand` when `exampleCondition` changes to `true`
 		// new Trigger(exampleSubsystem::exampleCondition)
 		// 		.onTrue(new ExampleCommand(exampleSubsystem));
-
 		// Schedule `exampleMethodCommand` when the Xbox controller's B button is pressed,
 		// cancelling on release.
 		// driverController.b().whileTrue(exampleSubsystem.exampleMethodCommand());
+
 		// driverJoystick.button(3).onTrue(toggleFieldRelative);
-		driverJoystick.button(11).onTrue(stopCommand);
+		// driverJoystick.button(11).onTrue(stopCommand);
 
 		// Test bindings
 		driverJoystick.button(1).onTrue(setModule);
 		driverJoystick.button(2).onTrue(zeroModule);
-		driverJoystick.button(5).onTrue(driveDistanceTest);
-		driverJoystick.button(6).onTrue(driveDurationTest);
-		driverJoystick.button(4).onTrue(rotateTest);
+
+		// driverJoystick.button(5).onTrue(driveDistanceTest);
+		// driverJoystick.button(6).onTrue(driveDurationTest);
+		// driverJoystick.button(4).onTrue(rotateTest);
 
 	}
 
