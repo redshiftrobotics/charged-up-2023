4450c0628324c837f3c253b569c632967bb9557d
JohndisHaver <121990902+JohndisHaver@users.noreply.github.com>
2023-03-01 15:58:38 -0800

    Creation of claw.java and start of imports
    imported all the necessary for creation of the claw subsystem.

---
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -0,0 +1,8 @@
+package frc.robot.subsystems;
+
+import com.revrobotics.AbsoluteEncoder;
+import com.revrobotics.CANSparkMax;
+import com.revrobotics.CANSparkMaxLowLevel.MotorType;
+
+import edu.wpi.first.wpilibj2.command.SubsystemBase;
+import edu.wpi.first.wpilibj2.command.CommandBase;
