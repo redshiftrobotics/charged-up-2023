758fecf8c284f99ef31b1018ab3b3776f7286688
Josh <58442585+JoshFarkas@users.noreply.github.com>
2023-03-01 17:42:31 -0800

    Create TestAuto
    Co-Authored-By: malochite <121986501+malochite@users.noreply.github.com>

--- src/main/java/frc/robot/commands/Autos.java
+++ src/main/java/frc/robot/commands/Autos.java
@@ -7,11 +7,18 @@ package frc.robot.commands;
 import frc.robot.subsystems.ExampleSubsystem;
 import edu.wpi.first.wpilibj2.command.CommandBase;
 import edu.wpi.first.wpilibj2.command.Commands;
+import edu.wpi.first.wpilibj2.command.SequentialCommandGroup;
+import frc.robot.subsystems.SwerveDrivetrain;
+import autos.testAuto
 
 public final class Autos {
 	/** Example static factory for an autonomous command. */
-	public static CommandBase exampleAuto(ExampleSubsystem subsystem) {
-		return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+	// public static CommandBase exampleAuto(ExampleSubsystem subsystem) {
+	// 	return Commands.sequence(subsystem.exampleMethodCommand(), new ExampleCommand(subsystem));
+	// }
+
+	public static SequentialCommandGroup auto1(SwerveDrivetrain drivetrain) {
+		return new testAuto(drivetrain);
 	}
 
 	private Autos() {
