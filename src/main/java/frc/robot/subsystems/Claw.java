20e660b6806215b507b8b7d7707265de3dc0b919
JohndisHaver <121990902+JohndisHaver@users.noreply.github.com>
2023-03-13 15:42:36 -0700

    Updated claw subsystem comments
    added aditional helpful information in claw subsystem
    
    Co-Authored-By: Arnold-Tian <75269639+Arnold-Tian@users.noreply.github.com>

--- src/main/java/frc/robot/subsystems/Claw.java
+++ src/main/java/frc/robot/subsystems/Claw.java
@@ -5,7 +5,7 @@ import com.revrobotics.CANSparkMaxLowLevel.MotorType;
 
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
-//makes up the claw subsystem & functions
+//makes up the claw subsystem & claw functions as telling the claw to move and connecting it to its motor
 public class Claw extends SubsystemBase {
 
 	private final CANSparkMax clawMotor;
