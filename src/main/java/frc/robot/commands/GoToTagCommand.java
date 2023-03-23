d4de7317278e21f1b281b6cb7fe2afc477b01ce7
michael-lesirge <100492377+michael-lesirge@users.noreply.github.com>
2023-03-22 17:28:35 -0700

    fix merge conflicts

--- src/main/java/frc/robot/commands/GoToTagCommand.java
+++ src/main/java/frc/robot/commands/GoToTagCommand.java
@@ -3,9 +3,7 @@ package frc.robot.commands;
 import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Transform3d;
 import edu.wpi.first.math.geometry.Translation2d;
-import frc.robot.Constants.CameraConstants;
 import frc.robot.subsystems.SwerveDrivetrain;
-import edu.wpi.first.networktables.Topic;
 
 public class GoToTagCommand extends RotateAndDriveSimultaneousCommand {
 	SwerveDrivetrain drivetrain;
@@ -18,7 +16,8 @@ public class GoToTagCommand extends RotateAndDriveSimultaneousCommand {
 		// GenericSubscriber sub = NetworkTablesInstance.getDefault().getTable("Vision")
 		// 		.getTransform3dTopic("aprilTagPosition").subscribe(new Transform3d());
 		// Transform3d aprilTag = sub.get();
-		Transform3d tagPose = aprilTag.plus(CameraConstants.CAMERA_POSITION);
+		// Transform3d tagPose = aprilTag.plus(CameraConstants.CAMERA_POSITION);
+		Transform3d tagPose = aprilTag;
 		Rotation2d tagRotation = new Rotation2d(-tagPose.getRotation().getY());
 		Translation2d driveDistance = new Translation2d(tagPose.getX(), tagPose.getZ());
 		driveDistance = driveDistance.plus(desiredDistToTag.rotateBy(tagRotation));
