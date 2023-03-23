67bbb16450a1914cd14ed0b97952130a2df018c9
Aleah H <aleahjham@gmail.com>
2023-03-23 12:51:23 -0700

    Change jakeclaw to victorspx

--- src/main/java/frc/robot/subsystems/JakeClaw.java
+++ src/main/java/frc/robot/subsystems/JakeClaw.java
@@ -1,21 +1,22 @@
 package frc.robot.subsystems;
 
+import com.ctre.phoenix.motorcontrol.VictorSPXControlMode;
+
 import edu.wpi.first.wpilibj.motorcontrol.MotorControllerGroup;
-import edu.wpi.first.wpilibj.motorcontrol.VictorSP;
+import edu.wpi.first.wpilibj.motorcontrol.Victor;
+import com.ctre.phoenix.motorcontrol.can.VictorSPX;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 //makes up the claw subsystem & claw functions as telling the claw to move and connecting it to its motor
 public class JakeClaw extends SubsystemBase {
 
-	private final VictorSP clawMotor1;
-	private final VictorSP clawMotor2;
-	private final MotorControllerGroup clawMotors;
+	private final VictorSPX clawMotor1;
+	private final VictorSPX clawMotor2;
 	private double clawSpeed;
 
 	public JakeClaw(int clawMotor1Id, int clawMotor2Id) {
-		clawMotor1 = new VictorSP(clawMotor1Id);
-		clawMotor2 = new VictorSP(clawMotor2Id);
-		clawMotors = new MotorControllerGroup(clawMotor1, clawMotor2);
+		clawMotor1 = new VictorSPX(clawMotor1Id);
+		clawMotor2 = new VictorSPX(clawMotor2Id);
 	}
 
 	//Setting the direction of the claw, called using the commands in RobotContainer through SetClawDirection
@@ -26,7 +27,8 @@ public class JakeClaw extends SubsystemBase {
 	//Periodically setting the claw according to the claw direction
 	@Override
 	public void periodic() {
-		clawMotors.set(clawSpeed);
+		clawMotor1.set(VictorSPXControlMode.PercentOutput, clawSpeed);
+		clawMotor2.set(VictorSPXControlMode.PercentOutput, clawSpeed);
 	}
 
 }
